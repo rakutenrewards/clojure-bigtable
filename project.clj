7520c8c083b346806efec7fd08d48ee137b5ba07
Connor Clark <connor.clark@rakuten.com>
2020-11-04 11:25:36 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-bigtable "0.1.0-SNAPSHOT"
+  :description "Clojure interface to Java BigTable client"
+  :url "https://github.com/RakutenReady/clojure-bigtable"
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns clojure-bigtable.core})
