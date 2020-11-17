4050192b5e31f47a8938d6280683cb98f8f970fa
Connor Clark <connor.clark@rakuten.com>
2020-11-17 16:35:33 -0500

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-bigtable "0.1.0"
+(defproject clojure-bigtable "0.1.1-SNAPSHOT"
   :description "Clojure interface to Java BigTable client"
   :url "https://github.com/RakutenReady/clojure-bigtable"
   :dependencies [[org.clojure/clojure "1.10.1"]
